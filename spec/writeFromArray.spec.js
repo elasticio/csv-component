eee2fa8ca90f4c2b17d9ba8637dad76a08807a27
Jacob Horbulyk <jacob@elastic.io>
2020-11-26 17:15:38 +0100

    Add breaking test.

--- spec/writeFromArray.spec.js
+++ spec/writeFromArray.spec.js
@@ -38,12 +38,18 @@ describe('CSV Write From Array component', function () {
         { put_url: 'https://examlple.mock/putUrl', get_url: 'https://examlple.mock/getUrl' });
 
     nock('https://examlple.mock')
-      .put('/putUrl')
+      .put('/putUrl', 'name;email;age;key1;not an age;not an age at all\n'
+          + 'Bob;bob@email.domain;30;1;;\n'
+          + 'Joe;joe@email.domain;11;;;322')
       .reply(200, {});
   });
 
   beforeEach(() => {
     emit = sinon.spy();
+  });
+
+  afterEach(() => {
+    expect(nock.isDone()).to.be.true;
   });
 
   it('should write csv rows', async () => {
@@ -78,5 +84,69 @@ describe('CSV Write From Array component', function () {
     }, msg, cfg);
 
     expect(emit.getCalls().length).to.equal(2);
+  });
+
+  it('should handle two messages rows', async () => {
+    nock('https://api.elastic.io', { encodedQueryParams: true })
+      .post('/v2/resources/storage/signed-url')
+      .reply(200,
+        { put_url: 'https://examlple.mock/putUrl2', get_url: 'https://examlple.mock/getUrl2' });
+
+    nock('https://examlple.mock')
+      .put('/putUrl2', 'foo;fooz\n'
+            + 'bar1;barz1\n'
+            + 'bar1;barz1')
+      .reply(200, {});
+
+    await write.init.call({
+      logger,
+    }, cfg);
+
+    const msg1 = {
+      body: {
+        inputArray: [
+          {
+            name: 'Bob',
+            email: 'bob@email.domain',
+            age: 30,
+            key1: true,
+            'not an age': null,
+            'not an age at all': undefined,
+          },
+          {
+            name: 'Joe',
+            email: 'joe@email.domain',
+            age: 11,
+            'not an age at all': 322,
+          },
+        ],
+      },
+    };
+
+    const msg2 = {
+      body: {
+        inputArray: [
+          {
+            foo: 'bar1',
+            fooz: 'barz1',
+          },
+          {
+            foo: 'bar2',
+            fooz: 'barz2',
+          },
+        ],
+      },
+    };
+
+    await write.process.call({
+      emit,
+      logger,
+    }, msg1, cfg);
+    await write.process.call({
+      emit,
+      logger,
+    }, msg2, cfg);
+
+    expect(emit.getCalls().length).to.equal(4);
   });
 });
